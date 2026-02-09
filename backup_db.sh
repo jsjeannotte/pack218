913bf87ac7a33ea89a73f3ddb12d6c66f4689a99
Jean-Sebastien Jeannotte <jjeannotte@netflix.com>
2026-02-08 21:59:02 -0800

    Fix backup script

--- backup_db.sh
+++ backup_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 timestamp=$(date +%Y%m%d_%H%M%S)
-tar -czf "database_backup_${timestamp}.tgz" database.db database.db-shm database.db-wal 2>/dev/null
+tar -czf "database_backup_${timestamp}.tgz" database.db database.db-shm database.db-wal
 echo "Backup created: database_backup_${timestamp}.tgz"
